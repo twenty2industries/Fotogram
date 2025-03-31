661863ecc9abb3ecc72d4f0d8767e4755da67209
twenty2industries <burak.bostanli.1@web.de>
2025-03-31 19:39:55 +0200

    Modified style.css - overlay backgrounds now use background-size: contain; picture IDs are now only used for rendering images.

--- fotogram-project/renderImage.js
+++ fotogram-project/renderImage.js
@@ -1,11 +1,12 @@
-const contentRef = document.getElementById('collageArea');
+const contentRef = document.getElementById("collageArea");
 
-function renderImages () {
-  contentRef.innerHTML = `        <div class="photoAlbumInput" id="pictureOne"></div>
-        <div class="photoAlbumInput" id="pictureTwo"></div>
-        <div class="photoAlbumInput" id="pictureThree"></div>
-        <div class="photoAlbumInput" id="pictureFour"></div>
-        <div class="photoAlbumInput" id="pictureFive"></div>
+function renderImages() {
+  contentRef.innerHTML = `
+    <div class="photoAlbumInput" id="pictureOne"></div>
+    <div class="photoAlbumInput" id="pictureTwo"></div>
+    <div class="photoAlbumInput" id="pictureThree"></div>
+    <div class="photoAlbumInput" id="pictureFour"></div>
+    <div class="photoAlbumInput" id="pictureFive"></div>
         <div class="photoAlbumInput" id="pictureSix"></div>
         <div class="photoAlbumInput" id="pictureSeven"></div>
         <div class="photoAlbumInput" id="pictureEight"></div>
