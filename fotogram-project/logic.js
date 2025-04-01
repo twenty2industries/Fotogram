276ee9c648086e4b40729f963c3f1dc815454614
twenty2industries <burak.bostanli.1@web.de>
2025-04-01 02:12:26 +0200

    A new variable currentIndex was declared and initialized with null. It will later be redefined to the clicked index in the array. This value is needed for the nextPicture() and previousPicture() functions. The removeDuplicate(event) function was edited to fix an issue where clicking on the buttonArea would reset duplicateElements. The issue was resolved with an if statement that checks if the toggleId contains the event.target and returns if true. All functions, except for toggling d_none, are working. Media query and favicon are missing.

--- fotogram-project/logic.js
+++ fotogram-project/logic.js
@@ -2,6 +2,7 @@ let myAlbum = Array.from(document.querySelectorAll(".photoAlbumInput"));
 const mainSection = document.getElementById("mainSection");
 const collageSection = document.getElementById("collageArea");
 let duplicateElements = [];
+let currentIndex = null;
 const toggleId = document.getElementById("buttonArea");
 
 for (let index = 0; index < myAlbum.length; index++) {
@@ -16,6 +17,7 @@ function removeAllOverlays() {
 
 function toggleOverlay(event) {
   removeAllOverlays();
+  currentIndex = myAlbum.indexOf(event.target);
   const eventDuplicate = event.target.cloneNode(true);
   eventDuplicate.classList.add("overlayCollage");
   contentRef.appendChild(eventDuplicate);
@@ -23,7 +25,28 @@ function toggleOverlay(event) {
   event.stopPropagation();
 }
 
+function nextPicture() {
+  removeAllOverlays(); // reset ist immer notwendig
+  currentIndex = (currentIndex + 1) % myAlbum.length;
+  const nextImage = myAlbum[currentIndex].cloneNode(true);
+  nextImage.classList.add("overlayCollage");
+  contentRef.appendChild(nextImage);
+  duplicateElements.push(nextImage);
+}
+
+function previousPicture() {
+  removeAllOverlays(); // reset ist immer notwendig
+  currentIndex = (currentIndex - 1 + myAlbum.length) % myAlbum.length;
+  const prevImage = myAlbum[currentIndex].cloneNode(true);
+  prevImage.classList.add("overlayCollage");
+  contentRef.appendChild(prevImage);
+  duplicateElements.push(prevImage);
+}
+
 function removeDuplicate(event) {
+  if (toggleId.contains(event.target)) {
+    return; // blocked die removeAllOverlay() ausführung notwendig
+  }
   if (!contentRef.contains(event.target)) {
     removeAllOverlays();
     toggleId.classList.remove("d_none");
@@ -32,12 +55,15 @@ function removeDuplicate(event) {
       toggleId.classList.remove("d_none");
       removeAllOverlays();
     }
-
   }
 }
 
 mainSection.addEventListener("click", removeDuplicate);
 collageSection.addEventListener("click", removeDuplicate);
 
-//    toggleId.classList.add('d_none');
-//      toggleId.classList.remove('d_none');
+// console.log(currentIndex); // angeklickter index
+// console.log(eventDuplicate); // angeklickter dom wert
+// console.log(duplicateElements); // array wert mit geklicktem datentyp
+// toggleId.classList.add('d_none');
+// toggleId.classList.remove('d_none');
+// console.log(myAlbum); //  array mit 12 datentyp länge
--- fotogram-project/renderButton.js
+++ fotogram-project/renderButton.js
@@ -2,9 +2,9 @@ const renderButtonArea = document.getElementById("buttonArea");
 
 function renderButton() {
   renderButtonArea.innerHTML = `
-    <button id='buttonMinus'>zurück</button>
+    <button id='buttonMinus' onclick="previousPicture()">zurück</button>
     <div class="inputArea"></div>
-    <button id='buttonPlus'>weiter</button>
+    <button id='buttonPlus' onclick="nextPicture()">weiter</button>
 `;
 }
 renderButton();
